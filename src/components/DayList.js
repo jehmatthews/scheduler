cda96f74c2ee21e2802f16534f78b1ff6bf347bc
jehmatthews <jehmatthews30@gmail.com>
2022-12-05 17:19:25 -0800

    completed interviewerlist and listitem functions with given styling. refactored both of these functions as well as the daylist functions. all implemented and working properly.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,15 +2,15 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const days = props.days.map((day) => {
+  const days = props.days.map(day => {
     return (
       <ul>
         <DayListItem
         key={day.id}
         name={day.name}
         spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
+        selected={day.name === props.value}
+        setDay={props.onChange}
         />
       </ul>
     );
