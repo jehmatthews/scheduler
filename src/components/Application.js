d7bef5da9d9db526fd6172b16320936ccc67b13b
jehmatthews <jehmatthews30@gmail.com>
2022-12-11 18:20:34 -0800

    implemented axios to setDays to get data from server and took away the days variable

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+import axios from "axios";
 
 const appointments = {
   "1": {
@@ -42,26 +43,15 @@ const appointments = {
   }
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/days`)
+    .then(response => {
+      setDays(response.data)
+    });
+  })
 
   return (
     <main className="layout">
