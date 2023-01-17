34084ac32dc95a41f961ebc617d2a28aef70cca4
jehmatthews <jehmatthews30@gmail.com>
2023-01-17 13:41:42 -0800

    dowloaded cypress testing package and implemented tests, testing app functionalities

--- stories/index.js
+++ stories/index.js
@@ -160,7 +160,7 @@ storiesOf("Appointment", module)
   ))
   .add("Confirm", () => (
     <Confirm
-      message="Delete the appointment?"
+      message="Are you sure you would like to delete?"
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
